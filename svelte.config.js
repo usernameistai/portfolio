e4cefc8308650a368f4a374ff3b94cc1a104ae5c
David Battye <battye_david@yahoo.co.uk>
2024-10-10 19:41:44 +0100

    blah

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import fs from 'node:fs';
 // import { vitePreprocess } from '@sveltejs/kit/vite';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
