9042ca85a4861e56170e380903c5809ad1430298
David Battye <battye_david@yahoo.co.uk>
2024-10-10 15:17:34 +0100

    first commit

---
+++ src/components/Footer.svelte
@@ -0,0 +1,24 @@
+<footer class="py-20 sm:py-32 bg-black border-t border-solid border-violet-950
+flex flex-col gap-4 sm:gap-8 justify-center items-center">
+    <p class="px-4 py-2 bg-white text-slate-950 font-medium">
+        Connect with me &darr;
+    </p>
+    <div class="flex flex-col gap-4 items-center justify-center">
+        <p><b class="pr-2">Email</b> battye_david@yahoo.co.uk</p>
+        <p><b class="pr-2">GitHub <i class="fa-brands fa-square-github"></i></b>
+            <a class="text-violet-400" href="https://github.com/usernameistai" target="_blank">
+                David Battye <sup> <span class="text-xs scale-75 pl-0.5">
+                    <i class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"></i>
+                </span> </sup>
+            </a>
+        </p>
+        <p><b class="pr-2">LinkedIn <i class="fa-brands fa-linkedin"></i></b>
+            <a class="text-violet-400" href="https://www.linkedin.com/in/david-battye-9932665a/" target="_blank">
+                David Battye <sup> <span class="text-xs scale-75 pl-0.5">
+                    <i class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]"></i>
+                </span> </sup>
+            </a>
+        </p>
+    </div>
+
+</footer>
---
+++ src/components/Main.svelte
@@ -0,0 +1,151 @@
+<script>
+    import Step from "./Step.svelte";
+
+    let steps = [
+        {name: 'PeakyBlogger', icon: 'fa-solid fa-tree', href: 'https://peakyblogger-d362176b187f.herokuapp.com/'},
+        {name: 'HelpMe-Car', icon: 'fa-solid fa-hands', href: 'https://helpme-car.herokuapp.com/'},
+        {name: 'StockMarket-WishList', icon: 'fa-solid fa-money-bill-trend-up', href: 'https://stockmarket-wishlist.onrender.com/'},
+    ];
+
+    let benefits = [
+        {name: 'a self taught developer', description: 'I taught myself to code using free online resources and absolutely fell in love with the creativity and problem solving that is involved in developing and engineering innovative new online experiences. Starting of with JavaScript, HTML & CSS and evolving my knowledge base to include JavaScript frameworks, backend programming, design, cloud services and much much more.'},
+        {name: 'a product design and UX fanatic', description: 'Carefully crafting and designing amazing user experiences allows me to express and experiment with every morsel of creativity I have. I love the challenge of learning new design concepts and enabling users with amazing online experiences.'},
+        {name: 'an excellent communicator', description: 'Communication is key and it is a paramount value of mine. I believe in transparency and constructive communication above all else. This helps me develop deep relationships and ensures my effectiveness and productivity in any work space with any team.'},
+    ]
+
+</script>
+
+<main class="flex flex-col flex-1 p-4">
+    <section 
+        id="introPage" 
+        class="grid grid-cols-1 lg:grid-cols-2 gap-10 py-8 sm:py-14"
+    >
+        <div 
+            class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10"
+        >
+            <h2 class="font-semibold text-4xl sm:text-5xl md:text-6xl">
+                Hi!! I'm <span class="poppins text-violet-400">David</span> Battye <br/>Full Stack
+                <span class="poppins text-violet-400">Developer</span>
+            </h2>
+            <p class="text-base sm:text-lg md:text-xl">
+                My <span class="text-violet-400">favourite tech</span> includes Javascript (Vite.js, React.js or SvelteKit), TailWindCSS, Node.js + Express.js, Heroku & Render
+            </p>
+            <a 
+                href="#" 
+                target="_blank"
+                class="blueShadow mx-auto lg:mr-auto lg:ml-0 text-base sm:text-lg md:text-xl poppins relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950 cursor-pointer">
+                <div class="absolute top-0 right-full w-full h-full bg-violet-400 opacity-20 group-hover:translate-x-full z-0 duration-200" />
+                <h4 class="relative z-9">Get in touch &rarr;</h4>
+            </a>
+        </div>
+        <div class="relative shadow-2xl grid place-items-center">
+            <img src={"images/prof.png"} alt="Profile Image" class="object-cover z-[2] max-h-[70vh]">
+        </div>
+    </section>
+
+
+
+    <section id="projects" class="py-20 lg:py-32 flex flex-col gap-24">
+        <div class="flex flex-col gap-2 text-center">
+            <h6 class="text-lg sm:text-xl md:text-2xl">
+                A few of my creative endeavours
+            </h6>
+            <h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
+                Curious to <span class="poppins text-violet-400">see</span> my work?
+            </h3>
+        </div>
+        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank" 
+            class="mx-auto px-4 py-2 rounded-md border border-solid border-white 
+            flex items-center gap-2 -mb-4 sm:-mb-0 -mt-10 hover:border-violet-700 duration-200">
+                <i class="fa-regular fa-circle-play"></i>
+                <p>Watch the video</p>
+        </a>
+        <div class="grid grid-cols-1 lg:grid-cols-3 gap-12 lg:gap-10">
+            <Step step={steps[0]}>
+                <p>PeakyBlogger is a Full Stack blogsite for my Father, created a <strong class="text-violet-400">MongoDb Atlas, EJS, Passport, Vanilla JS, Express.js + Node.js</strong> CRUD application. Hosted on <strong class="text-violet-400">Heroku.</strong> This is so he can document his walks and eateries of the Peak District to promote healthy living and eating as well as wellbeing.</p>
+            </Step>
+            <Step step={steps[1]}>
+                <p>HelpMe-Car is a Full Stack web app to enable helping other people's cars, created a <strong class="text-violet-400">MongoDb Atlas, Express.js, React.js + Node.js</strong> CRUD application. Hosted on <strong class="text-violet-400">Heroku.</strong>Here you can enter helpful suggestions from pre-written examples to a database to help individuals maintain their cars safely.</p>
+            </Step>
+            <Step step={steps[2]}>
+                <p>StockMarket-WishList is a Static web app to help you keep track of your favourite stock using NASDAQ100. Created using <strong class="text-violet-400">Vite.js & TailwindCSS.</strong>This is hosted on <strong class="text-violet-400">Render.</strong> The symbol used will bring up the stock of choice into the graph area of the app where you can select the relevant data you are after.</p>
+            </Step>
+        </div>
+    </section>
+    <section id="about" 
+        class="py-20 pt-10 lg:pt-16 lg:py-32 flex flex-col gap-16 sm:gap-20 md:gap-24 relative"
+    >
+        <div class="flex flex-col gap-2 text-center relative before:absolute before:top-0 
+        before:left-0 before:w-2/3 before:h-1.5 before:bg-violet-700 after:absolute after:bottom-0 
+        after:right-0 after:w-2/3 after:h-1.5 after:bg-violet-700 py-4">
+            <h6 class="text-lg sm:text-xl md:text-2xl">Want to know more?</h6>
+            <h3 class="font=semibold text-3xl sm:text-4xl md:text-5xl">
+                A bit <span class="poppins text-violet-400">about</span> me.
+            </h3>
+        </div>
+        <p class="mx-auto poppins font-semibold text-lg sm:text-xl md:text-2xl"> I am . . .</p>
+        <div class="flex flex-col gap-20 w-full mx-auto maz-w-[800px]">
+            {#each benefits as benefit, index}
+                <div class="flex gap-6 sm:gap-8">
+                    <p class="poppins text-4xl sm:text-5xl md:text-6xl text-slate-500 font-semibold">
+                        0{index + 1}
+                    </p>
+                    <div class="flex flex-col gap-6 sm:gap-8">
+                        <h3 class="text-2xl sm:text-3xl md:text-5xl">
+                            {benefit.name}
+                        </h3>
+                        <p>{benefit.description}</p>
+                    </div>
+                </div>
+            {/each}
+        </div>
+        <h5 class="text-2xl sm:text-3xl font-semibold text-center poppins">The <span class="text-violet-400 poppins">Complete </span> Package</h5>
+        <div class="flex flex-col overflow-x-scroll gap-10 max-w-[800px] mx-auto w-full">
+            <table class="bg-white text-slate-700 rounded text-center">
+                <thead class="border-b border-solid border-slate-200">
+                    <tr class="">
+                        <th/>
+                        <th class="whitespace-nowrap p-2 px-4">Candidate #1</th>
+                        <th class="whitespace-nowrap p-2 px-4">Candidate #2</th>
+                        <th class="whitespace-nowrap p-2 px-4">Candidate #3</th>
+                        <th class="whitespace-nowrap bg-violet-700 text-white p-4 px-8">Me</th>
+                    </tr>
+                </thead>
+                <tbody class="">
+                    <tr class="border-b border-solid border-slate-200">
+                        <td class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm whitespace-nowrap">Dedication</td>
+                        <td><i class="fa-solid fa-xmark text-slate-500"></i></td>
+                        <td><i class="fa-solid fa-check text-slate-500"></i></td>
+                        <td><i class="fa-solid fa-xmark text-slate-500"></i></td>
+                        <td><i class="fa-solid fa-check text-green-500"></i></td>
+                    </tr>
+                    <tr class="border-b border-solid border-slate-200">
+                        <td class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm whitespace-nowrap">Critical Thought</td>
+                        <td><i class="fa-solid fa-xmark text-slate-500"></i></td>
+                        <td><i class="fa-solid fa-check text-slate-500"></i></td>
+                        <td><i class="fa-solid fa-check text-slate-500"></i></td>
+                        <td><i class="fa-solid fa-check text-green-500"></i></td>
+                    </tr>
+                    <tr class="border-b border-solid border-slate-200">
+                        <td class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm whitespace-nowrap">Interpersonal Skills</td>
+                        <td><i class="fa-solid fa-check text-slate-500"></i></td>
+                        <td><i class="fa-solid fa-check text-slate-500"></i></td>
+                        <td><i class="fa-solid fa-xmark text-slate-500"></i></td>
+                        <td><i class="fa-solid fa-check text-green-500"></i></td>
+                    </tr>
+                    <tr class="border-b border-solid border-slate-200">
+                        <td class="border-r border-solid border-white pl-4 pr-8 py-4 font-semibold text-sm whitespace-nowrap">Programming Ability</td>
+                        <td><i class="fa-solid fa-check text-slate-500"></i></td>
+                        <td><i class="fa-solid fa-xmark text-slate-500"></i></td>
+                        <td><i class="fa-solid fa-check text-slate-500"></i></td>
+                        <td><i class="fa-solid fa-check text-green-500"></i></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <div class="mx-auto -mt-12 italic sm:hidden opacity-50">
+            <p>Scroll to see more &rarr;</p>
+        </div>
+        <p class="mx-auto">So why not invest?</p>
+    </section>
+</main>
