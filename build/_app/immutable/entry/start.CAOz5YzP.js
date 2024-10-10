4adc9dd39c656badc163f6bcb00b3e2c8b7494b2
David Battye <battye_david@yahoo.co.uk>
2024-10-10 15:39:46 +0100

    second commit to get a build folder

---
+++ build/_app/immutable/entry/start.CAOz5YzP.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.Bb_VL6tJ.js";export{t as start};
--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,16 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter()
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: '200.html'
+    })
+  },
+  paths: {
+    base: process.env.BASE_PTH || '',
   },
   preprocess: vitePreprocess()
 };
