e5a6d554daf9e0ba1d6a21efe0b496d3a2369e68
David Battye <battye_david@yahoo.co.uk>
2024-10-11 11:23:54 +0100

    try again with netlify account

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,31 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import fs from 'fs';
-// import { vitePreprocess } from '@sveltejs/kit/vite';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter({
-      // out: 'build',
-      pages: 'build',
-      assets: 'build',
-      fallback: null,
-      precompress: false,
-      strict: false
-    }),
+    adapter: adapter()
   },
   preprocess: vitePreprocess()
 };
 export default config;
+
+
+// import adapter from '@sveltejs/adapter-static';
+// import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+// import fs from 'fs';
+// // import { vitePreprocess } from '@sveltejs/kit/vite';
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+//   kit: {
+//     adapter: adapter({
+//       // out: 'build',
+//       pages: 'build',
+//       assets: 'build',
+//       fallback: null,
+//       precompress: false,
+//       strict: false
+//     }),
+//   },
+//   preprocess: vitePreprocess()
+// };
+// export default config;
